ce3094e8a9cc1be9c36a0999f561b2c3c979b346
André Diego Piske <andrepiske@gmail.com>
2020-03-16 19:48:11 +0100

    Add documentation to DataView

--- ext/llc/dataview.c
+++ ext/llc/dataview.c
@@ -43,6 +43,33 @@ t_dv_allocator(VALUE klass) {
   return Data_Wrap_Struct(klass, t_dv_gc_mark, t_dv_free, dv);
 }
 
+/*
+ * call-seq:
+ *  initialize(buffer, offset, size, endianess:)
+ *
+ * Constructs a new DataView that provides a view of the data in a ByteBuffer.
+ *
+ * Example:
+ *   buffer = ByteBuffer.new(10)
+ *   buffer[1] = 20
+ *   buffer[2] = 55
+ *   view = DataView.new(buffer, 1, endianess: :little)
+ *   view.getU8() == ( 20 & (55 << 8) ) # true
+ *
+ * Or:
+ *
+ *   view = DataView.new(buffer, 1) # default endianess is "big endian"
+ *   view.getU8() == ( (20 << 8) | 55 ) # true
+ *
+ * @param buffer [ByteBuffer] The byte buffer where to operate
+ * @param offset [Integer] Optional. The byte offset from the byte buffer. The
+ *   default value is zero
+ * @param size [Integer] Optional. The size in bytes that this DataView can
+ *   see into the byte buffer. If left blank, the current size of the byte
+ *   buffer will be used
+ * @param endianess [:big, :little] Optional. The default value is big
+ *
+ */
 static VALUE
 t_dv_initialize(int argc, VALUE *argv, VALUE self) {
   DECLAREDV(self);
@@ -132,6 +159,52 @@ t_dv_each(VALUE self) {
   return self;
 }
 
+/*
+ * Defined the new offset for the DataView.
+ *
+ * Changing the offset does not affect the size.
+ * If passed a negative value, it will be summed with the underlying buffer
+ * size. The final value must not be lower than zero.
+ *
+ * @param offset [Integer] The new offset
+ */
+static VALUE
+t_dv_setoffset(VALUE self, VALUE offset) {
+  DECLAREDV(self);
+  DECLAREBB(dv->bb_obj);
+  int offset_val = NIL_P(offset) ? 0 : NUM2INT(offset);
+  if (offset_val < 0)
+    offset_val += (int)bb->size;
+  if (offset_val < 0)
+    rb_raise(rb_eArgError, "calculated offset is negative: %d", offset_val);
+
+  dv->offset = (unsigned long)offset_val;
+  return self;
+}
+
+/*
+ * Defined the new offset for the DataView.
+ *
+ * Changing the size does not affect the offset.
+ * If passed a negative value, it will be summed with the underlying buffer
+ * size. The final value must not be lower than zero.
+ *
+ * @param size [Integer] The new size. Must be zero or greater
+ */
+static VALUE
+t_dv_setsize(VALUE self, VALUE size) {
+  DECLAREDV(self);
+  DECLAREBB(dv->bb_obj);
+  int size_val = NIL_P(size) ? (int)bb->size : NUM2INT(size);
+  if (size_val < 0)
+    size_val += (int)bb->size;
+  if (size_val < 0)
+    rb_raise(rb_eArgError, "calculated size is negative: %d", size_val);
+
+  dv->size = (unsigned long)size_val;
+  return self;
+}
+
 #define DECLARENCHECKIDX(index) int idx = NUM2INT(index); \
   if (idx < 0) idx += (int)dv->size; \
   if (idx < 0 || idx >= dv->size) rb_raise(rb_eArgError, "index out of bounds: %d", idx);
@@ -139,6 +212,15 @@ t_dv_each(VALUE self) {
 #define CHECKBOUNDSBB(v) if ((v) < 0 || (v) >= (bb)->size) \
   rb_raise(rb_eArgError, "index out of underlying buffer bounds: %d", (v));
 
+/*
+ * Reads a bit at index.
+ *
+ * This method differs from the other in that the +index+ is given in bits, not
+ * in bytes.
+ * Thus it's possible to read from index +0+ to +(size * 8) - 1+
+ *
+ * @return [Integer] Either 0 or 1
+ */
 static VALUE
 t_dv_getbit(VALUE self, VALUE index) {
   DECLAREDV(self); DECLAREBB(dv->bb_obj);
@@ -156,6 +238,11 @@ t_dv_getbit(VALUE self, VALUE index) {
   return (bb->ptr[byte_idx] & bit_mask) ? UINT2NUM(1) : UINT2NUM(0);
 }
 
+/*
+ * Reads the byte at index as an +unsigned char+
+ *
+ * @return [Integer] Integer between 0 and 255
+ */
 static VALUE
 t_dv_getu8(VALUE self, VALUE index) {
   DECLAREDV(self); DECLARENCHECKIDX(index); DECLAREBB(dv->bb_obj);
@@ -164,6 +251,11 @@ t_dv_getu8(VALUE self, VALUE index) {
   return UINT2NUM(bb->ptr[real_idx]);
 }
 
+/*
+ * Reads two bytes starting at index as an +unsigned short+
+ *
+ * @return [Integer] Integer between 0 and 65535
+ */
 static VALUE
 t_dv_getu16(VALUE self, VALUE index) {
   DECLAREDV(self); DECLARENCHECKIDX(index); DECLAREBB(dv->bb_obj);
@@ -182,6 +274,11 @@ t_dv_getu16(VALUE self, VALUE index) {
   return UINT2NUM(val);
 }
 
+/*
+ * Reads three bytes starting at index as a 3 bytes long unsigned integer
+ *
+ * @return [Integer] Integer between 0 and 16777215
+ */
 static VALUE
 t_dv_getu24(VALUE self, VALUE index) {
   DECLAREDV(self); DECLARENCHECKIDX(index); DECLAREBB(dv->bb_obj);
@@ -203,6 +300,11 @@ t_dv_getu24(VALUE self, VALUE index) {
   return UINT2NUM(val);
 }
 
+/*
+ * Reads four bytes starting at index as a 4 bytes long unsigned integer
+ *
+ * @return [Integer] Integer between 0 and 4294967295
+ */
 static VALUE
 t_dv_getu32(VALUE self, VALUE index) {
   DECLAREDV(self); DECLARENCHECKIDX(index); DECLAREBB(dv->bb_obj);
@@ -229,6 +331,12 @@ t_dv_getu32(VALUE self, VALUE index) {
 
 #define ADJUSTBOUNDS(val, max) if (val < 0) val = 0; else if (val > max) val = max;
 
+/*
+ * Interprets one byte at index and assigns it an unsigned value
+ *
+ * Values lower than zero will be set to 0 and values greater than 255
+ * will be capped.
+ */
 static VALUE
 t_dv_setu8(VALUE self, VALUE index, VALUE value) {
   DECLAREDV(self); DECLARENCHECKIDX(index); DECLAREBB(dv->bb_obj);
@@ -240,6 +348,12 @@ t_dv_setu8(VALUE self, VALUE index, VALUE value) {
   return self;
 }
 
+/*
+ * Interprets two bytes starting at index and sets them an unsigned value
+ *
+ * Values lower than zero will be set to 0 and values greater than 65535
+ * will be capped.
+ */
 static VALUE
 t_dv_setu16(VALUE self, VALUE index, VALUE value) {
   DECLAREDV(self); DECLARENCHECKIDX(index); DECLAREBB(dv->bb_obj);
@@ -260,6 +374,12 @@ t_dv_setu16(VALUE self, VALUE index, VALUE value) {
   return self;
 }
 
+/*
+ * Interprets three bytes starting at index and sets them an unsigned value
+ *
+ * Values lower than zero will be set to 0 and values greater than 16777215
+ * will be capped.
+ */
 static VALUE
 t_dv_setu24(VALUE self, VALUE index, VALUE value) {
   DECLAREDV(self); DECLARENCHECKIDX(index); DECLAREBB(dv->bb_obj);
@@ -283,6 +403,12 @@ t_dv_setu24(VALUE self, VALUE index, VALUE value) {
   return self;
 }
 
+/*
+ * Interprets four bytes starting at index and sets them an unsigned value
+ *
+ * Values lower than zero will be set to 0 and values greater than 4294967295
+ * will be capped.
+ */
 static VALUE
 t_dv_setu32(VALUE self, VALUE index, VALUE value) {
   DECLAREDV(self); DECLARENCHECKIDX(index); DECLAREBB(dv->bb_obj);
